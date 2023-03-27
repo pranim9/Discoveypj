64ca4299cdd2efc141f16995bb9d76995141040a
Pranim <48821638+pranim9@users.noreply.github.com>
2023-03-27 21:25:33 +0530

    Add files via upload

---
+++ app/webApi.php
@@ -0,0 +1,99 @@
+
+
+<?php 
+
+
+
+if(isset($_REQUEST['id'])){
+
+
+
+
+
+   if($_REQUEST['id']=='95'){
+
+        echo                              '{"status":"success","code":200,"message":"Channels List Loaded Successfully","data":                          {"id":"95","title":"Discovery Channel","logo":"https://i.postimg.cc/3wYj6cmS/Discovery.png","category":"Infotainment","language":"English","playurl":"http://localhost:8080/t1/t1/app/api/1.m3u8"}}';
+
+        
+}
+                 if($_REQUEST['id']=='96'){
+
+        echo                              '{"status":"success","code":200,"message":"Channels List Loaded Successfully","data":                          {"id":"96","title":"Discovery Science","logo":"https://i.postimg.cc/x1SLJ4xf/Discovery-Science.png","category":"Infotainment","language":"English","playurl":"http://localhost:8080/t1/t1/app/api/2.m3u8"}}';
+
+  }               if($_REQUEST['id']=='97'){
+
+        echo                              '{"status":"success","code":200,"message":"Channels List Loaded Successfully","data":                          {"id":"97","title":"TLC","logo":"https://i.postimg.cc/GmrXDQqC/TLC-HD.png","category":"Lifestyle","language":"English","playurl":"http://localhost:8080/t1/t1/app/api/3.m3u8"}}';
+
+ }                if($_REQUEST['id']=='98'){
+
+        echo                              '{"status":"success","code":200,"message":"Channels List Loaded Successfully","data":                          {"id":"98","title":"Discovery Turbo","logo":"https://i.postimg.cc/5NmwhMqh/Discovery-Turbo.png","category":"Infotainment","language":"English","playurl":"http://localhost:8080/t1/t1/app/api/4.m3u8"}}';
+
+  }               if($_REQUEST['id']=='99'){
+
+        echo                              '{"status":"success","code":200,"message":"Channels List Loaded Successfully","data":                          {"id":"99","title":"Animal Planet","logo":"https://i.postimg.cc/LsWkxBBh/Animal-Planet-HD.png","category":"Infotainment","language":"English","playurl":"http://localhost:8080/t1/t1/app/api/5.m3u8"}}';
+
+  }               if($_REQUEST['id']=='101'){
+
+        echo                              '{"status":"success","code":200,"message":"Channels List Loaded Successfully","data":                          {"id":"101","title":"Investigation Discovery","logo":"https://i.postimg.cc/ydK43sq1/Investigation-Discovery.png","category":"Infotainment","language":"English","playurl":"http://localhost:8080/t1/t1/app/api/6.m3u8"}}';
+
+  }               if($_REQUEST['id']=='106'){
+
+        echo                              '{"status":"success","code":200,"message":"Channels List Loaded Successfully","data":                          {"id":"106","title":"Eurosport","logo":"https://i.postimg.cc/QdZb3tcd/Eurosports.png","category":"Sports","language":"English","playurl":"http://localhost:8080/t1/t1/app/api/7.m3u8"}}';
+
+   }              if($_REQUEST['id']=='109'){
+
+        echo                              '{"status":"success","code":200,"message":"Channels List Loaded Successfully","data":                          {"id":"109","title":"DMAX","logo":"https://i.postimg.cc/Fsw9stKR/DMAX.png","category":"Lifestyle","language":"English","playurl":"http://localhost:8080/t1/t1/app/api/8.m3u8"}}';
+
+   }              if($_REQUEST['id']=='110'){
+
+        echo                              '{"status":"success","code":200,"message":"Channels List Loaded Successfully","data":                          {"id":"110","title":"Travel Channel","logo":"https://i.postimg.cc/XvQmqFg8/Travel-Channel.png","category":"Lifestyle","language":"English","playurl":"http://localhost:8080/t1/t1/app/api/9.m3u8"}}';
+               
+}
+
+if($_REQUEST['id']=='111'){
+
+        echo                              '{"status":"success","code":200,"message":"Channels List Loaded Successfully","data":                          {"id":"111","title":"HGTV","logo":"https://i.postimg.cc/3NqZ32y3/HGTV.png","category":"Lifestyle","language":"English","playurl":"http://localhost:8080/t1/t1/app/api/10.m3u8"}}';
+               
+}
+
+
+
+   if($_REQUEST['id']=='112'){
+
+        echo                              '{"status":"success","code":200,"message":"Channels List Loaded Successfully","data":                          {"id":"112","title":"Food Network","logo":"https://i.postimg.cc/Nj0PVLt2/Food-Network.png","category":"Lifestyle","language":"English","playurl":"http://localhost:8080/t1/t1/app/api/11.m3u8"}}';
+               
+}
+
+
+if($_REQUEST['id']=='113'){
+
+        echo                              '{"status":"success","code":200,"message":"Channels List Loaded Successfully","data":                          {"id":"113","title":"Discovery Kids","logo":"https://i.postimg.cc/mk58WyBc/Discovery-Kids.png","category":"Kids","language":"English","playurl":"http://localhost:8080/t1/t1/app/api/12.m3u8"}}';
+               
+}
+
+ 
+               
+
+
+    
+    
+    
+    
+    
+    
+    
+
+}else{
+
+    echo '{"status":"success","code":200,"message":"Channels List Loaded Successfully","data":{"count":16,"list":
+[{"id":"95","title":"Discovery Channel","logo":"https://i.postimg.cc/3wYj6cmS/Discovery.png","category":"Infotainment","language":"English"},{"id":"96","title":"Discovery Science","logo":"https://i.postimg.cc/x1SLJ4xf/Discovery-Science.png","category":"Infotainment","language":"English"},{"id":"97","title":"TLC","logo":"https://i.postimg.cc/GmrXDQqC/TLC-HD.png","category":"Lifestyle","language":"English"},{"id":"98","title":"Discovery Turbo","logo":"https://i.postimg.cc/5NmwhMqh/Discovery-Turbo.png","category":"Infotainment","language":"English"},{"id":"99","title":"Animal Planet","logo":"https://i.postimg.cc/LsWkxBBh/Animal-Planet-HD.png","category":"Infotainment","language":"English"},{"id":"101","title":"Investigation Discovery","logo":"https://i.postimg.cc/ydK43sq1/Investigation-Discovery.png","category":"Infotainment","language":"English"},{"id":"106","title":"Eurosport","logo":"https://i.postimg.cc/QdZb3tcd/Eurosports.png","category":"Sports","language":"English"},{"id":"109","title":"DMAX","logo":"https://i.postimg.cc/Fsw9stKR/DMAX.png","category":"Lifestyle","language":"English"},{"id":"110","title":"Travel Channel","logo":"https://i.postimg.cc/XvQmqFg8/Travel-Channel.png","category":"Lifestyle","language":"English"},{"id":"111","title":"HGTV","logo":"https://i.postimg.cc/3NqZ32y3/HGTV.png","category":"Lifestyle","language":"English"},{"id":"112","title":"Food Network","logo":"https://i.postimg.cc/Nj0PVLt2/Food-Network.png","category":"Lifestyle","language":"English"},
+{"id":"113","title":"Discovery Kids","logo":"https://i.postimg.cc/mk58WyBc/Discovery-Kids.png","category":"Kids","language":"English"}]}}';
+
+}
+
+
+
+?>
+
+
+
